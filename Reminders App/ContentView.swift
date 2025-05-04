28ed551f22e01c9265e3933603463af963a39a11
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-05-04 16:13:32 +1000

    Initial Commit

---
+++ Reminders App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reminders App
+//
+//  Created by Armand Packham-McGuiness on 4/5/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reminders App/Reminders_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminders_AppApp.swift
+//  Reminders App
+//
+//  Created by Armand Packham-McGuiness on 4/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct Reminders_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
