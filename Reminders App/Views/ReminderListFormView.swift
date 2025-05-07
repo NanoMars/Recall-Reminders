3d56cbe89e9855304bd57f9d14bf557bb9853644
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-05-07 22:05:18 +1000

    i've done stuff towards sorted views yippeeee

---
+++ Reminders App/Views/ReminderListFormView.swift
@@ -0,0 +1,31 @@
+//
+//  ReminderListFormView.swift
+//  Reminders App
+//
+//  Created by Armand Packham-McGuiness on 7/5/2025.
+//
+
+
+//
+//  ReminderListFormView
+//  Reminders App
+//
+//  Created by Armand Packham-McGuiness on 7/5/2025.
+//
+
+import SwiftUI
+
+
+struct ReminderListFormView: View {
+    var body: some View {
+        
+        
+    }
+
+}
+
+
+
+#Preview {
+    ReminderListFormView()
+}
