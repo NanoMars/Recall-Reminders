dfc4cb74366e2382515aa9ba03d83cd4fb56eb8c
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-11 14:44:05 +1000

    Icons for export

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Armand Packham-McGuiness on 8/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ recall reminders/recall_remindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  recall_remindersApp.swift
+//  recall reminders
+//
+//  Created by Armand Packham-McGuiness on 10/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct recall_remindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
